6e0c210b3605b2308c547afcc4bb2dd0a999139d
Ameya Purao <ameyapurao1@gmail.com>
2021-05-09 23:00:11 -0500

    Updated scripts

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+ 
 public class MenuButtons : MonoBehaviour
 {
     public void quit()
--- Assets/Scripts/MissileMovement.cs
+++ Assets/Scripts/MissileMovement.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class MissileMovement : MonoBehaviour
 {
     public float speed = 30f;
